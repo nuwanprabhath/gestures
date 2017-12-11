3da40ad1dff8b0c8efa4feb2bac260c6cb73932c
nuwan <nuwan.munasinghe@trycake.com>
2017-12-11 23:50:20 +0530

    Adding background removal and merging multiple frames.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import numpy as np
-import cv2;
+import cv2
 
-print(cv2.__version__);
+print(cv2.__version__)
 
 
 def flip_frame(frame_to_flip):
@@ -10,30 +10,47 @@ def flip_frame(frame_to_flip):
 
 # Giving file name will read from video file instead from camera
 capture = cv2.VideoCapture(0)
-
+# Removing background https://docs.opencv.org/3.3.0/db/d5c/tutorial_py_bg_subtraction.html
+fgbg = cv2.createBackgroundSubtractorMOG2(history=50,
+                                          detectShadows=True)  # detectShadows=True can be added as a parameter
+initial = True
 # Define the codec and create VideoWriter object to save video to file
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))
+c=0
 while True:
     if capture.isOpened():
         # Capture frame by frame
         ret, frame = capture.read()
         if ret:
-            # print(capture.get(3)) Width
-            # print(capture.get(4)) Height
+            width = round(capture.get(3))
+            height = round(capture.get(4))
+            # print('width'+str(width))
+            # print('height'+str(height))
             flippedFrame = flip_frame(frame)
-            # Operations on the frame come here
+
+            frameWithoutBackground = fgbg.apply(flippedFrame)
             gray = cv2.cvtColor(flippedFrame, cv2.COLOR_BGR2GRAY)
-            # thresholdImage = cv2.adaptiveThreshold(cv2.medianBlur(gray, 11), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-            # Less noise
-            thresholdImage1 = cv2.adaptiveThreshold(cv2.medianBlur(gray, 11), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-            # thresholdImage = cv2.adaptiveThreshold(cv2.GaussianBlur(gray, (11, 11), 0), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-            # thresholdImage = cv2.adaptiveThreshold(cv2.bilateralFilter(gray,5,75,75), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-            # Write to file
-            # out.write(flippedFrame)
-            # Display resulting frame
-            # cv2.imshow('frame', thresholdImage)
-            cv2.imshow('frame1', thresholdImage1)
+            ret, thresh1 = cv2.threshold(cv2.medianBlur(frameWithoutBackground, 11), 0, 1,
+                                         cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+            if initial:
+                merged_frame = np.ones([height, width], np.uint8)
+                initial = False
+            print("-----"+str(c))
+
+            print(merged_frame)
+
+            if c > 0:
+                merged_frame = np.bitwise_and(merged_frame, thresh1)
+                print("++++-----")
+                print(thresh1)
+                print("after")
+                print(merged_frame)
+            c=c+1
+            cv2.imshow('frame1', np.multiply(merged_frame, 255))
+            cv2.imshow('frame2', np.multiply(thresh1, 255))
+
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
